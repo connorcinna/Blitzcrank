e77250ef9d23bf512c01d9d2208f97db05159a37
connorcinna <connorcinna@gmail.com>
2021-07-08 23:52:00 -0400

    updated gsearch some more

--- Commands/gsearch.js
+++ Commands/gsearch.js
@@ -1,11 +1,25 @@
 const discord = require("discord.js");
 const fetch = require("node-fetch");
 module.exports.run = async (bot, message, args) => {
-        var url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCDvi2YxuEsz5uxR1e1h6gq2iF9Ly_WPZU&cx=71446e05228ee4314&q=" + args + "&searchType=image&fileType=jpg&alt=json";
+        var str = args.toString().replace(/,'/g, "");
+        console.log('parameter passed: ' + str);
+        var url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCDvi2YxuEsz5uxR1e1h6gq2iF9Ly_WPZU&cx=71446e05228ee4314&q=" + str + "&searchType=image&fileType=jpg&alt=json";
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                if (data.error.code === 429) {
+                console.log(data);
+                console.log(data.searchInformation);
+                if (data.searchInformation.totalResults != 0) {
+                    try {
+                        message.channel.send(data.items[1].link);
+                    } catch (error) {
+                        message.channel.send("shut the fuck up weeb");
+                    } 
+                }
+                else  {
+                    message.channel.send({files: ["./Commands/no search results lol.png"]});
+                }
+                if (data.error != undefined && data.error.code === 429) {
                     var curr = new Date();
                     var curr_arr = [];
                     curr_arr[0] = curr.getHours();
@@ -21,17 +35,7 @@ module.exports.run = async (bot, message, args) => {
                     var time_string = time_diff[0] + ":" + time_diff[1] + ":" + time_diff[2];
                     message.channel.send("search quota reached for the day, new searches available in " + time_string);
                 }
-                console.log(data);
-                if (data.searchInformation.totalResults != 0) {
-                    try {
-                        message.channel.send(data.items[1].link);
-                    } catch (error) {
-                        message.channel.send("shut the fuck up weeb");
-                    } 
-                }
-                else  {
-                    message.channel.send({files: ["./Commands/no search results lol.png"]});
-                }
+                
         });
 }
 module.exports.help = {
