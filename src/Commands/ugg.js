f908969de15149474e4b6d7f91bc6c15b355b913
connorcinna <connorcinna@gmail.com>
2022-02-22 22:30:11 -0500

    working on integrating canvas for ugg

--- src/Commands/ugg.js
+++ src/Commands/ugg.js
@@ -2,6 +2,7 @@ const discord = require("discord.js");
 const fetch = require("node-fetch");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const canvas = require("canvas"); 
 module.exports.run = async (bot, message, args) => {
     try { 
         var url;
@@ -30,41 +31,81 @@ module.exports.run = async (bot, message, args) => {
         else if (args[1] != null && args[1] == "mid") {
             url = "https://u.gg/lol/champions/" + args[0] + "/build?role=middle";
         }
+        else if (args[1] != null && args[1] == "supp") {
+            url = "https://u.gg/lol/champions/" + args[0] + "/build?role=support";
+        }
         else url = "https://u.gg/lol/champions/" + args[0] + "/build";
+
         const {data} = await axios.get(url);
         const $ = cheerio.load(data);
         const runes_primary = [];
         const runes_secondary = [];
         const skill_path = [];
         const items = [];
-        let skills = $("div[class='skill-priority-path']");
-        $(skills).find("div[class='skill-label bottom-center']").each((i, element) =>  {
-            var skill = $(element).text();
-            skill_path.push(skill);
-        });
-        let keystone = $("div[class='perk keystone perk-active']").find('img').attr('src');
-        runes_primary.push(keystone);
-        const primary_tree = $("div[class='rune-tree_v2 primary-tree']");
-        $(primary_tree).find("div[class='perk perk-active']").each((i, element) => { 
-            if (i > 2) return false; //returning false is how Cheerio wants the .each loop to terminate
-            var perk = $(element).find('img').attr('src');
-            runes_primary.push(perk);
-        });
+        const item_canvas = canvas.createCanvas(300, 40);
+        const context = item_canvas.getContext('2d');
+        var skill_attachment;
+        function skill_fill(skill_path) {
+            let skills = $("div[class='skill-priority-path']");
+            $(skills).find("div[class='champion-skill-with-label']").each((i, element) =>  {
+                if (i>2) return false; 
+                var skill = $(element).find('img').attr('src');
+                console.log(skill);
+                skill_path.push(skill);
+            });
+        }
+        function draw_skill_path(skill_path, skill_attachment) {
+            var img_0 = new Image()
+            var img_1 = new Image()
+            var img_2 = new Image()
 
-        const secondary_tree = $("div[class='secondary-tree']");
-        $(secondary_tree).find("div[class='perk perk-active']").each((i, element) => { 
-            if (i > 1) return false;
-            var perk = $(element).find('img').attr('src');
-            runes_secondary.push(perk);
-        });
-        var skill_path_string = skill_path.join(' => ');
-        const item_path = $("div[class='content-section content-section_no-padding recommended-build_items media-query media-query_DESKTOP_MEDIUM__DESKTOP_LARGE']");
-        $(item_path).find("div[class='item-img']").each((i, element) => {
-           items.push($(this).css('background-image'); 
-        });
+            let skill_0 = canvas.loadImage(skill_path[0]);
+            context.drawImage(skill_0, 0, 0, 36, item_canvas.height);
+            let skill_1 = canvas.loadImage(skill_path[1]);
+            context.drawImage(skill_1, 72, 0, 108, item_canvas.height);
+            let skill_2 = canvas.loadImage(skill_path[2]);
+            context.drawImage(skill_2, 108, 0, 140, item_canvas.height);
+            skill_attachment = new MessageAttachment(item_canvas.toBuffer())
+        }
+        function runes_fill(runes_primary, runes_secondary) {
+            let keystone = $("div[class='perk keystone perk-active']").find('img').attr('src');
+            runes_primary.push(keystone);
+            let primary_tree = $("div[class='rune-tree_v2 primary-tree']");
+            $(primary_tree).find("div[class='perk perk-active']").each((i, element) => { 
+                if (i > 2) return false; //returning false is how Cheerio wants the .each loop to terminate
+                var perk = $(element).find('img').attr('src');
+                runes_primary.push(perk);
+            });
+
+            let secondary_tree = $("div[class='secondary-tree']");
+            $(secondary_tree).find("div[class='perk perk-active']").each((i, element) => { 
+                if (i > 1) return false;
+                var perk = $(element).find('img').attr('src');
+                runes_secondary.push(perk);
+            });
+        }
+        function items_fill(items) {
+            let item_path = $("div[class='content-section content-section_no-padding recommended-build_items media-query media-query_DESKTOP_MEDIUM__DESKTOP_LARGE']");
+            $(item_path).find("div[class='item-img']").each((i, element) => {
+                let element_string = $(element).html();
+                let result_start = element_string.indexOf('(')+1;
+                let result_end = element_string.indexOf(')');
+                let result = element_string.substring(result_start, result_end);
+                console.log(result);
+                items.push(result);
+            });    
+        }
+        //var skill_path_string = skill_path.join(' => ');
+
+        skill_fill(skill_path);
+        draw_skill_path(skill_path, skill_attachment);
+        runes_fill(runes_primary, runes_secondary);
+        items_fill(items);
         console.log(items);
-        message.channel.send(skill_path_string, {
+
+        message.channel.send({
             files: [
+                skill_attachment,
                 runes_primary[0],
                 runes_primary[1],
                 runes_primary[2],
@@ -75,7 +116,7 @@ module.exports.run = async (bot, message, args) => {
         });
     }
     catch (error) {
-        throw error;
+        console.log(error);
     }
 };
 module.exports.name = "ugg";
